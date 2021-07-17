10ba43a9c5ed6d86f918dd10044504367060ed7f
Gajendiran, Gnanasekaran | RASIA <gnanasekaran.gajendi@rakuten.com>
2021-07-17 13:08:56 +0800

    PR test

--- app/test_app.py
+++ app/test_app.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-
 from .app import app
 
 client = TestClient(app)
@@ -8,3 +7,9 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.text == "UP"
